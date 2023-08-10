23c876d8e88d5cc15ec79e0e3430b6107349b444
Naptive <10virtus21@gmail.com>
2023-08-10 13:03:20 +0200

    new

--- config/FireBase.js
+++ config/FireBase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoq9utnlZd-qfnRBAAbR88I-GOIwZQyFc",
-  authDomain: "letro-469f0.firebaseapp.com",
-  projectId: "letro-469f0",
-  storageBucket: "letro-469f0.appspot.com",
-  messagingSenderId: "693531912824",
-  appId: "1:693531912824:web:58a9097b1dcce8967f1b8d",
-  measurementId: "G-T19RPLL94R"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
